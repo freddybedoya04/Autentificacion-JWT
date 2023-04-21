8830f47db4ba7b151b50e41368f52493e2454cd8
Freddy Bedoya <freddybedoya04@gmail.com>
2023-04-21 12:33:39 -0500

    se realiza creacion de servicion de autentificacion con modificaciones a los html de registros y logica para verificar si ya esta un usuario o no.

--- src/app/modules/auth/components/register-form/register-form.component.ts
+++ src/app/modules/auth/components/register-form/register-form.component.ts
@@ -5,36 +5,84 @@ import { faEye, faEyeSlash } from '@fortawesome/free-solid-svg-icons';
 
 import { CustomValidators } from '@utils/validators';
 
+import {AuthService} from '@services/auth.service';
+import {RequestStatus} from '@models/request-status.model'
+
 @Component({
   selector: 'app-register-form',
   templateUrl: './register-form.component.html',
 })
 export class RegisterFormComponent {
+  formUser=this.formBuilder.nonNullable.group({
+    email: ['', [Validators.email, Validators.required]]
+  })
   form = this.formBuilder.nonNullable.group({
     name: ['', [Validators.required]],
     email: ['', [Validators.email, Validators.required]],
-    password: ['', [Validators.minLength(6), Validators.required]],
+    password: ['', [Validators.minLength(8), Validators.required]],
     confirmPassword: ['', [Validators.required]],
   }, {
     validators: [ CustomValidators.MatchValidator('password', 'confirmPassword') ]
   });
-  status: string = 'init';
+  status: RequestStatus = 'init';
   faEye = faEye;
   faEyeSlash = faEyeSlash;
   showPassword = false;
+  showRegister=false;
 
   constructor(
     private formBuilder: FormBuilder,
-    private router: Router
+    private router: Router,
+    private _auth:AuthService
   ) {}
 
   register() {
     if (this.form.valid) {
       this.status = 'loading';
       const { name, email, password } = this.form.getRawValue();
-      console.log(name, email, password);
+      this._auth.register(email, password,name).subscribe({
+        next:()=>{
+          this.status='success';
+          this.router.navigate(['/login'])
+        },
+        error:(error)=>{
+          this.status='failed';
+          console.log(error )
+        }
+      })
+
     } else {
       this.form.markAllAsTouched();
+    }
+  }
+  isAvailable(){
+    debugger;
+    if(this.formUser.valid){
+      this.status='loading';
+
+      const { email} = this.formUser.getRawValue();
+      this._auth.isAvailable(email).subscribe({
+        next:(res)=>{
+          if(!res.isAvailable){
+            this.status='success';
+            this.router.navigate(['/login'],{
+              queryParams:{email}
+            })
+          }else{
+            this.status='success';
+            this.showRegister=true;
+            this.form.controls.email.setValue(email);
+          }
+
+        },
+        error:(error)=>{
+          this.status='failed';
+          console.log(error )
+        }
+      })
+
+    } else {
+      this.formUser.markAllAsTouched();
     }
   }
 }
